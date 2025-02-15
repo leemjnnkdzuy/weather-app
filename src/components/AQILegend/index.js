6d5ea8d20716c9687d28125b6c06fe11c646fcf0
LeeMjnnkDzuy <duylelv17@gmail.com>
2025-02-15 22:32:49 +0700

    refactor AQILegend and AirQualityTable styles; enhance responsiveness and localization support for air quality labels

--- src/components/AQILegend/index.js
+++ src/components/AQILegend/index.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import classNames from "classnames/bind";
 import styles from "./AQILegend.module.scss";
+import { useTranslation } from "react-i18next";
 
 const cx = classNames.bind(styles);
 
 function AQILegend({ onRangeSelect, selectedRanges }) {
 	const [isSticky, setIsSticky] = useState(false);
+	const { t } = useTranslation();
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -18,37 +20,49 @@ function AQILegend({ onRangeSelect, selectedRanges }) {
 	}, []);
 
 	const legends = [
-		{ range: "0-50", label: "Tốt", class: "bg-green", min: 0, max: 50 },
-		{ range: "51-100", label: "Trung bình", class: "bg-yellow", min: 51, max: 100 },
-		{
-			range: "101-150",
-			label: "Không tốt cho các nhóm nhạy cảm",
-			class: "bg-orange",
-			min: 101,
-			max: 150,
-		},
-		{
-			range: "151-200",
-			label: "Không lành mạnh",
-			class: "bg-red",
-			min: 151,
-			max: 200,
-		},
-		{
-			range: "201-300",
-			label: "Rất không tốt",
-			class: "bg-purple",
-			min: 201,
-			max: 300,
-		},
-		{
-			range: "301+",
-			label: "Nguy hiểm",
-			class: "bg-maroon",
-			min: 301,
-			max: Infinity,
-		},
-	];
+	{
+		range: "0-50",
+		label: t("airQualityRanking.quality.good"),
+		class: "bg-green",
+		min: 0,
+		max: 50,
+	},
+	{
+		range: "51-100",
+		label: t("airQualityRanking.quality.moderate"),
+		class: "bg-yellow",
+		min: 51,
+		max: 100,
+	},
+	{
+		range: "101-150",
+		label: t("airQualityRanking.quality.unhealthySensitive"),
+		class: "bg-orange",
+		min: 101,
+		max: 150,
+	},
+	{
+		range: "151-200",
+		label: t("airQualityRanking.quality.unhealthy"),
+		class: "bg-red",
+		min: 151,
+		max: 200,
+	},
+	{
+		range: "201-300",
+		label: t("airQualityRanking.quality.veryUnhealthy"),
+		class: "bg-purple",
+		min: 201,
+		max: 300,
+	},
+	{
+		range: "301+",
+		label: t("airQualityRanking.quality.hazardous"),
+		class: "bg-maroon",
+		min: 301,
+		max: Infinity,
+	},
+];
 
 	return (
 		<div className={cx("wrapper", { sticky: isSticky })}>
